72c5766fdc4a4e30df9dc06b924fe6c8c7df1e0a
ayslla.gomes <04414927110@git.economia.gov.br>
2020-11-11 09:32:20 -0300

    Correção dos impactos, no momento de buildar o projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,56 +1,50 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
-
-import {InputTextModule} from 'primeng/inputtext';
-import {ButtonModule} from 'primeng/button';
-import {DropdownModule} from 'primeng/dropdown';
-import {ToolbarModule} from 'primeng/toolbar';
-import {TooltipModule} from 'primeng/tooltip';
-import {InputTextareaModule} from 'primeng/inputtextarea';
-
-
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { TableModule } from 'primeng/table';
-import { HeaderComponent } from './components/template/header/header.component';
-
-import { MeusPedidosComponent } from './components/template/meus-pedidos/meus-pedidos.component';
-import { FormsModule } from '@angular/forms';
-import { MessageComponent } from './message/message.component';
-import { NovoProdutoComponent } from './components/template/novo-produto/novo-produto.component';
-import { CadastrarEspecieComponent } from './components/template/cadastrar-especie/cadastrar-especie.component';
-import { CadastrarMarcaComponent } from './components/template/cadastrar-marca/cadastrar-marca.component';
 import { MarcaService } from './marca.service';
-import { HttpClientModule } from '@angular/common/http';
 import { EspecieService } from './especie.service';
-import { ProdutoService } from './produto.service';
-import { MeusProdutosComponent } from './components/template/meus-produtos/meus-produtos.component';
-import { RouterModule, Routes } from '@angular/router';
+import { ProdutoService } from './shared/service/produto.service';
 
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+import { TooltipModule } from 'primeng/tooltip';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { AppRoutingModule } from './app-routing.module';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+
+import { MessageComponent } from './message/message.component';
+// import { HeaderComponent } from './components/template/header/header.component';
+// import { MeusPedidosComponent } from './components/template/meus-pedidos/meus-pedidos.component';
+// import { NovoProdutoComponent } from './components/template/novo-produto/novo-produto.component';
+// import { MeusProdutosComponent } from './components/template/meus-produtos/meus-produtos.component';
+// import { CadastrarMarcaComponent } from './components/template/cadastrar-marca/cadastrar-marca.component';
+// import { CadastrarEspecieComponent } from './components/template/cadastrar-especie/cadastrar-especie.component';
 
 const routes: Routes = [
-  { path: 'produtos', component: MeusProdutosComponent},
-  { path: 'produtos/novo', component: NovoProdutoComponent},
-  { path: 'pedidos', component: MeusPedidosComponent},
-  { path: 'marcas', component: CadastrarMarcaComponent},
-  { path: 'especies', component: CadastrarEspecieComponent}
+  // { path: 'produtos', component: MeusProdutosComponent },
+  // { path: 'produtos/novo', component: NovoProdutoComponent },
+  // { path: 'pedidos', component: MeusPedidosComponent },
+  // { path: 'marcas', component: CadastrarMarcaComponent },
+  // { path: 'especies', component: CadastrarEspecieComponent }
 ];
-
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    MeusPedidosComponent,
-    NovoProdutoComponent,
-    MeusProdutosComponent,
     MessageComponent,
-    CadastrarEspecieComponent,
-    CadastrarMarcaComponent
+    // HeaderComponent,
+    // MeusPedidosComponent,
+    // NovoProdutoComponent,
+    // MeusProdutosComponent,
+    // CadastrarEspecieComponent,
+    // CadastrarMarcaComponent
   ],
   imports: [
     FormsModule,
@@ -66,10 +60,12 @@ const routes: Routes = [
     HttpClientModule,
     RouterModule.forRoot(routes)
   ],
-  providers: [MarcaService,
-              EspecieService,
-              ProdutoService],
-              
-  bootstrap: [AppComponent]
+  providers: [
+    MarcaService,
+    EspecieService,
+    ProdutoService
+  ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/shared/service/produto.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProdutoService {
+  apiURL = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) {}
+
+  consultar(): Observable<any> {
+    return this.http.get(`${this.apiURL}/produtos`);
+  }
+
+  adicionar(produto: any) {
+    this.http.post(`${this.apiURL}/produtos`, produto)
+      .subscribe(
+        resultado => {
+          console.log(resultado);
+        },
+        erro => {
+          if (erro.status === 400) {
+            console.log(erro);
+          }
+        }
+      );
+  }
+
+  excluir(id: number) {
+    this.http.delete(`${this.apiURL}/produtos/${id}`)
+      .subscribe(
+        resultado => {
+          console.log('Produto excluído com sucesso.');
+        },
+        erro => {
+          if (erro.status === 404) {
+            console.log('Produto não localizado.');
+          }
+        }
+      );
+  }
+
+}
