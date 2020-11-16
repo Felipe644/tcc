b05866a68c4c4daee188da4b62c0fca632dacd42
ayslla.gomes <04414927110@git.economia.gov.br>
2020-11-16 09:49:30 -0300

    Escopo do Produto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,41 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
+import { ListProdutoComponent } from './components/produto/list/list.component';
 import { DemandListComponent } from './pages/demand-list/demand-list.component';
-import {HeaderComponent} from "./components/header/header.component";
-import { CadastrarEspecieComponent } from './components/cadastrar-especie/cadastrar-especie.component';
+import { NewProdutoComponent } from './components/produto/persist/new/new.component';
 import { CadastrarMarcaComponent } from './components/cadastrar-marca/cadastrar-marca.component';
+import { CadastrarEspecieComponent } from './components/cadastrar-especie/cadastrar-especie.component';
 
 const routes: Routes = [
 
-  // {
-  //   path: '',
-  //   component: HeaderComponent
-  // },
-  { 
-    path: '', 
-    component: DemandListComponent 
+  {
+    path: '',
+    component: DemandListComponent
   },
-
-  // {
-  //   path: 'produtos',
-  //   component: MeusProdutosComponent
-  // },
-  // {
-  //   path: 'produtos/novo',
-  //   component: NovoProdutoComponent
-  // },
+  {
+    path: 'produto',
+    component: ListProdutoComponent
+  },
+  {
+    path: 'produto/cadastrar',
+    component: NewProdutoComponent
+  },
   // {
   //   path: 'pedidos',
   //   component: MeusPedidosComponent
   // },
-   {
-     path: 'marcas',
-     component: CadastrarMarcaComponent
-   },
-   {
-     path: 'especies',
-     component: CadastrarEspecieComponent
-   }
+  {
+    path: 'marcas',
+    component: CadastrarMarcaComponent
+  },
+  {
+    path: 'especies',
+    component: CadastrarEspecieComponent
+  }
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,70 +1,62 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { CardModule } from 'primeng/card';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { ToolbarModule } from 'primeng/toolbar';
 import { TooltipModule } from 'primeng/tooltip';
 import { DropdownModule } from 'primeng/dropdown';
 import { InputTextModule } from 'primeng/inputtext';
+import { PanelMenuModule } from 'primeng/panelmenu';
 import { AppRoutingModule } from './app-routing.module';
 import { InputTextareaModule } from 'primeng/inputtextarea';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { MessageComponent } from './message/message.component';
-import { DemandListComponent } from './pages/demand-list/demand-list.component';
-import { CardModule } from 'primeng/card';
 import { CardComponent } from './components/card/card.component';
-import { TitleHeaderComponent } from './components/title-header/title-header.component';
 import { HeaderComponent } from './components/header/header.component';
-import { CadastrarEspecieComponent } from './components/cadastrar-especie/cadastrar-especie.component';
-import {PanelMenuModule} from 'primeng/panelmenu';
+import { ListProdutoComponent } from './components/produto/list/list.component';
+import { DemandListComponent } from './pages/demand-list/demand-list.component';
+import { NewProdutoComponent } from './components/produto/persist/new/new.component';
+import { TitleHeaderComponent } from './components/title-header/title-header.component';
 import { CadastrarMarcaComponent } from './components/cadastrar-marca/cadastrar-marca.component';
-// import { HeaderComponent } from './components/template/header/header.component';
-// import { MeusPedidosComponent } from './components/template/meus-pedidos/meus-pedidos.component';
-// import { NovoProdutoComponent } from './components/template/novo-produto/novo-produto.component';
-// import { MeusProdutosComponent } from './components/template/meus-produtos/meus-produtos.component';
-
+import { CadastrarEspecieComponent } from './components/cadastrar-especie/cadastrar-especie.component';
 
 @NgModule({
+  imports: [
+    FormsModule,
+    BrowserModule,
+    AppRoutingModule,
+    CardModule,
+    TableModule,
+    ButtonModule,
+    ToolbarModule,
+    TooltipModule,
+    BrowserModule,
+    DropdownModule,
+    InputTextModule,
+    PanelMenuModule,
+    HttpClientModule,
+    InputTextareaModule,
+    BrowserAnimationsModule
+  ],
   declarations: [
     AppComponent,
     MessageComponent,
     DemandListComponent,
     CardComponent,
-    TitleHeaderComponent,
     HeaderComponent,
-    CadastrarEspecieComponent,
+    NewProdutoComponent,
+    ListProdutoComponent,
+    TitleHeaderComponent,
     CadastrarMarcaComponent,
-    // MeusPedidosComponent,
-    // NovoProdutoComponent,
-    // MeusProdutosComponent,
-
-  ],
-  imports: [
-    FormsModule,
-    BrowserModule,
-    AppRoutingModule,
-    TableModule,
-    InputTextModule,
-    ButtonModule,
-    ToolbarModule,
-    TooltipModule,
-    CardModule,
-    InputTextareaModule,
-    DropdownModule,
-    HttpClientModule,
-    PanelMenuModule,
-    BrowserModule,
-    BrowserAnimationsModule,
-    
+    CadastrarEspecieComponent
   ],
   providers: [],
   bootstrap: [ AppComponent ]
 })
-export class AppModule {
-}
+export class AppModule {}
